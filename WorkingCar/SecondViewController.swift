d4657f34c13084d4d0b005e8ed7b80da6ec5c287
TakayukiMorijiri <takayuki.m1206@gmail.com>
2016-06-28 11:46:06 +0800

    not use storyboard

---
+++ WorkingCar/SecondViewController.swift
@@ -0,0 +1,52 @@
+//
+//  SecondViewController.swift
+//  WorkingCar
+//
+//  Created by 森尻尭之 on 2016/06/28.
+//  Copyright © 2016年 Takayuki Morijiri. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.greenColor()
+        
+        let btn: UIButton = UIButton()
+        btn.frame = CGRectMake(140, 100, 100, 100)
+        btn.backgroundColor = UIColor.blackColor()
+        btn.setTitle("次へ", forState: .Normal)
+        btn.addTarget(self, action: "gotoNext:", forControlEvents: .TouchUpInside)
+        self.view.addSubview(btn)
+
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func gotoNext(sender: UIButton){
+        
+        let secondView: ThirdViewController = ThirdViewController()
+        self.navigationController!.pushViewController(secondView, animated: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- WorkingCar/ViewController.swift
+++ WorkingCar/ViewController.swift
@@ -13,8 +13,40 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.whiteColor()
+        
+        let btn: UIButton = UIButton()
+        btn.frame = CGRectMake(60, 100, 200, 50)
+        btn.backgroundColor = UIColor.orangeColor()
+        btn.setTitle("あいうえお順", forState: .Normal)
+        btn.addTarget(self, action: "gotoSecond:", forControlEvents: .TouchUpInside)
+        self.view.addSubview(btn)
+        
+        let btn1: UIButton = UIButton()
+        btn1.frame = CGRectMake(60, 300, 200, 50)
+        btn1.backgroundColor = UIColor.blackColor()
+        btn1.setTitle("アイコン", forState: .Normal)
+        btn1.addTarget(self, action: "gotoThird:", forControlEvents: .TouchUpInside)
+        self.view.addSubview(btn1)
+        
     }
 
+    
+    func gotoSecond(sender: UIButton){
+        
+        let secondView: SecondViewController = SecondViewController()
+        self.navigationController!.pushViewController(secondView, animated: true)
+    }
+    
+    func gotoThird(sender: UIButton){
+        
+        let secondView: ThirdViewController = ThirdViewController()
+        self.navigationController!.pushViewController(secondView, animated: true)
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
